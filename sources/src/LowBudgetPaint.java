383f6203c1fd936008f50f4cb828aa61891a337a
DreamShokk <kristofilles9@gmail.com>
2022-07-10 22:00:50 +0200

    Something that resembles a working product-ish

--- sources/src/LowBudgetPaint.java
+++ sources/src/LowBudgetPaint.java
@@ -1,42 +1,83 @@
-import java.awt.image.BufferedImage;
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
 
+import javax.swing.event.MouseInputListener;
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class LowBudgetPaint extends JFrame{
-    final int width=800;
-    final int height=800;
-    final int menuheight=50;
 
-
-    BufferedImage currentImage=new BufferedImage(width, height-menuheight, BufferedImage.TYPE_INT_ARGB);
+    static final int width=800;
+    static final int height=800;
+    static final int menuheight=50;
+    static Color currentColor=Color.black; 
+    static int currentLineSize=10;
     
 
 
     public LowBudgetPaint(String title){
         super(title);
-
+        setLayout(null);
         setBounds( 0,0,width, height);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
         JPanel menu=new JPanel();
-        JPanel body=new JPanel();
+        Painel body=new Painel();
+        menu.setLayout(null);
+
         menu.setBounds(0, 0, width, menuheight);
         body.setBounds(0, menuheight, width, height-menuheight);
         menu.setBackground(Color.GRAY);
         body.setBackground(Color.WHITE);
+
+        JButton save=new JButton("Save image");
+        save.setBackground(Color.white);
+        save.setForeground(Color.black);
+        save.setBounds(0,0,50,50);
+        save.addMouseListener(new MouseInputListener() {
+            
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            try{
+                ImageIO.write(Painel.currentImage, "png", new File("image.png"));
+                repaint();
+            }catch(IOException ex){System.err.println("Error while ommiting file: "+ex.getMessage());};}
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+
+            @Override
+            public void mouseExited(MouseEvent e) {}
+
+            @Override
+            public void mouseDragged(MouseEvent e) {}
+
+            @Override
+            public void mouseMoved(MouseEvent e) {}
+            
+        });
+
+        menu.add(save);
+
 
         add(menu);
         add(body);
         setResizable(false);
         setVisible(true);
     }
-
-
-
-
-
 
     public static void main(String[] args) {
         new LowBudgetPaint("LBP");
---
+++ sources/src/Painel.java
@@ -0,0 +1,61 @@
+import javax.swing.JPanel;
+import javax.swing.event.MouseInputListener;
+
+import java.awt.image.BufferedImage;
+import java.awt.BasicStroke;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.Point;
+
+public class Painel extends JPanel{
+    static BufferedImage currentImage=new BufferedImage(LowBudgetPaint.width, LowBudgetPaint.height-LowBudgetPaint.menuheight, BufferedImage.TYPE_INT_ARGB);
+    static Point PP;
+
+    @Override
+    public void paintComponent(Graphics g){
+        g.drawImage(currentImage, 0, 0, null);
+    }
+    private void toDraw(MouseEvent e){
+        Graphics2D a=currentImage.createGraphics();
+        a.setColor(LowBudgetPaint.currentColor);
+        a.setStroke(new BasicStroke(LowBudgetPaint.currentLineSize,BasicStroke.CAP_ROUND,BasicStroke.JOIN_ROUND));
+        a.drawLine(PP.x, PP.y, e.getX(), e.getY());
+        repaint();
+    }
+    Painel(){
+        super();
+        MouseInputListener motion=new MouseInputListener() {
+            
+            @Override
+            public void mouseClicked(MouseEvent e) {}
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                PP=e.getPoint();
+                toDraw(e);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+
+            @Override
+            public void mouseExited(MouseEvent e) {}
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                toDraw(e);
+                PP=e.getPoint();
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {}
+            
+        };
+        addMouseListener(motion);
+        addMouseMotionListener(motion);
+    }
+} 
