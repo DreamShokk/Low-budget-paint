d56cd3b93b2fd90ce498da8de87fecf570315847
DreamShokk <kristofilles9@gmail.com>
2022-07-14 16:47:27 +0200

    Optimized button creation and menu building, added future tool buttons

--- sources/src/LowBudgetPaint.java
+++ sources/src/LowBudgetPaint.java
@@ -20,6 +20,7 @@ public class LowBudgetPaint extends JFrame{
     static JLabel sizeToText;
     static JLabel rubberSizeToText;
     static ArrayList<JPanel> panels=new ArrayList<>();
+    static ArrayList<ButtonPrototype> buttons=new ArrayList<>();
     static Tool currentTool=Tool.Brush;
 
 
@@ -28,6 +29,9 @@ public class LowBudgetPaint extends JFrame{
     static final ImageIcon done=new ImageIcon(LowBudgetPaint.class.getResource("images/done.png"));
     static final ImageIcon size=new ImageIcon(LowBudgetPaint.class.getResource("images/size.png"));
     static final ImageIcon rubber=new ImageIcon(LowBudgetPaint.class.getResource("images/rubber.png"));
+    static final ImageIcon bucket=new ImageIcon(LowBudgetPaint.class.getResource("images/bucket.png"));
+    static final ImageIcon spray=new ImageIcon(LowBudgetPaint.class.getResource("images/spray.png"));
+    static final ImageIcon wand=new ImageIcon(LowBudgetPaint.class.getResource("images/wand.png"));
     static final ImageIcon pipette=new ImageIcon(LowBudgetPaint.class.getResource("images/pipette.png"));
 
 //This instance
@@ -37,6 +41,9 @@ public class LowBudgetPaint extends JFrame{
         for(int i=0;i<panels.size();i++)
             if(i!=ExcludedIndex)
                 panels.get(i).setVisible(false);
+    }
+    public static void setActiveTool(){
+
     }
     private LowBudgetPaint(String title){
         //The window itself
@@ -54,7 +61,7 @@ public class LowBudgetPaint extends JFrame{
         menu.setBackground(Color.GRAY);
 
         //Save button on the menu
-        ButtonPrototype save=new ButtonPrototype(saved,0,new MouseAdapter() {
+        buttons.add(Tool.Save.code,new ButtonPrototype(saved,0,new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 hideAll(Tool.Save.code);
@@ -66,22 +73,21 @@ public class LowBudgetPaint extends JFrame{
                     Timer timer = new Timer(2000, arg0 -> ((JButton)e.getSource()).setIcon(saved));
                     timer.setRepeats(false);
                     timer.start();
-
-
                 }catch(IOException ex){System.err.println("Error while omitting file: "+ex.getMessage());}}
-        });
+        }) );
         panels.add(Tool.Save.code,new JPanel());
         //Button for brush settings
-        ButtonPrototype setBrush=new ButtonPrototype(size,1,new MouseAdapter() {
+        buttons.add(Tool.Brush.code, new ButtonPrototype(size,1,new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 hideAll(1);
                 currentTool=Tool.Brush;
                 panels.get(Tool.Brush.code).setVisible(!panels.get(Tool.Brush.code).isVisible());
-            }});
+            }}));
+
 
         //UI for setting brush details
-        panels.add(Tool.Brush.code,setBrush.getMenu(75,Color.black));
+        panels.add(Tool.Brush.code,buttons.get(Tool.Brush.code).getMenu(75,Color.black));
 
         JSlider setSizeSlider=new JSlider(5,25);
         sizeToText=new JLabel(setSizeSlider.getValue()+" px");
@@ -119,16 +125,16 @@ public class LowBudgetPaint extends JFrame{
         panels.get(Tool.Brush.code).add(colorPicker);
 
         //Button for rubber settings
-        ButtonPrototype setRubber=new ButtonPrototype(rubber, 2, new MouseAdapter() {
+        buttons.add(Tool.Rubber.code, new ButtonPrototype(rubber, Tool.Rubber.code, new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 hideAll(Tool.Rubber.code);
                 currentTool=Tool.Rubber;
                 panels.get(Tool.Rubber.code).setVisible(!panels.get(Tool.Rubber.code).isVisible());
-            }});
+            }}));
 
         //Panel for rubber settings
-        panels.add(setRubber.getMenu(50,Color.black));
+        panels.add(buttons.get(Tool.Rubber.code).getMenu(50,Color.black));
 
         //Slider for rubber size
         JSlider setRubberSlider=new JSlider(5,50);
@@ -149,19 +155,41 @@ public class LowBudgetPaint extends JFrame{
         panels.get(Tool.Rubber.code).add(rubberSizeToText);
 
         //TODO Use these enumerated panel slots when new functions are implemented
-        panels.add(Tool.Bucket.code,new JPanel());
-        panels.add(Tool.Spray.code,new JPanel());
-        panels.add(Tool.Wand.code,new JPanel());
+        //Bucket tool button and panel
+        buttons.add(Tool.Bucket.code, new ButtonPrototype(bucket, Tool.Bucket.code, new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                //TODO add mouseevent
+            }
+        }));
+        panels.add(Tool.Bucket.code,buttons.get(Tool.Bucket.code).getMenu(0,Color.WHITE));
+        //Spray tool button and panel
+        buttons.add(Tool.Spray.code, new ButtonPrototype(spray, Tool.Spray.code, new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                //TODO add mouseevent
+            }
+        }));
+        panels.add(Tool.Spray.code,buttons.get(Tool.Spray.code).getMenu(0,Color.WHITE));
+
+        //Wand tool button and panel
+        buttons.add(Tool.Wand.code, new ButtonPrototype(wand, Tool.Wand.code, new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                //TODO add mouseevent
+            }
+        }));
+        panels.add(Tool.Wand.code,buttons.get(Tool.Wand.code).getMenu(0,Color.WHITE));
 
 
         //Colorpicker (pipette) Button and menu
-        ButtonPrototype setPipette=new ButtonPrototype(pipette, Tool.ColorPicker.code, new MouseAdapter() {
+        buttons.add(Tool.ColorPicker.code,new ButtonPrototype(pipette, Tool.ColorPicker.code, new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 currentTool=Tool.ColorPicker;
             }
-        });
-        panels.add(Tool.ColorPicker.code,new JPanel());
+        }));
+        panels.add(Tool.ColorPicker.code,buttons.get(Tool.ColorPicker.code).getMenu(0,Color.WHITE));
 
 
 
@@ -176,10 +204,9 @@ public class LowBudgetPaint extends JFrame{
         body.setBackground(Color.WHITE);
 
         //Construction of the menu bar
-        menu.add(save);
-        menu.add(setBrush);
-        menu.add(setRubber);
-        menu.add(setPipette);
+        buttons.forEach(l->{
+            menu.add(l);
+        });
 
         //Construction of the window itself
         add(menu);
